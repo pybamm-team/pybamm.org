11b8e5222036603e901f4a3bc4a03b0dbe0b248c
arjxn.py <arjxn.py@gmail.com>
2023-10-21 04:14:26 +0530

    Make `themes` session

--- noxfile.py
+++ noxfile.py
@@ -3,3 +3,13 @@ import os
 import sys
 
 nox.options.reuse_existing_virtualenvs = True
+
+@nox.session(name="themes")
+def run_themes(session):
+    session.run("git",
+                "submodule",
+                "update",
+                "--init",
+                "--recursive",
+                external=True,
+            )
