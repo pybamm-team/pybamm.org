d1ff46b7025c8abe54a1cfddcee9d922a9f02e1d
arjxn.py <arjxn.py@gmail.com>
2023-10-21 04:25:44 +0530

    Do not clone themes if already exists

--- noxfile.py
+++ noxfile.py
@@ -6,10 +6,14 @@ nox.options.reuse_existing_virtualenvs = True
 
 @nox.session(name="themes")
 def run_themes(session):
-    session.run("git",
-                "submodule",
-                "update",
-                "--init",
-                "--recursive",
-                external=True,
-            )
+    themes_dir = "themes"
+    if not os.path.exists(themes_dir) or not os.listdir(themes_dir):
+        session.run("git",
+            "submodule",
+            "update",
+            "--init",
+            "--recursive",
+            external=True,
+        )
+    else:
+        print("'themes/' already exists in the project's root.")
